7502413a7e956d3634b0cfe3a65facbcad0f7121
CatherineChoque <85088235+CatherineChoque@users.noreply.github.com>
2021-09-28 16:45:30 -0500

    test de funciones para Api

--- src/api.js
+++ src/api.js
@@ -8,24 +8,24 @@ const rutaRelativaEjemplo = 'example/README.md';
 
 // si es ruta relativa cambia a absoluta
 const rutaAbsoluta = (ruta) => path.isAbsolute(ruta) ? ruta : path.resolve(ruta);
-console.log('La ruta es absoluta: ---> ', rutaAbsoluta(rutaAbsolutaEjemplo), 9);
-console.log('La ruta es absoluta: ---> ', rutaAbsoluta(rutaRelativaEjemplo), 10);
+//console.log('La ruta es absoluta: ---> ', rutaAbsoluta(rutaAbsolutaEjemplo), 9);
+//console.log('La ruta es absoluta: ---> ', rutaAbsoluta(rutaRelativaEjemplo), 10);
 
 //si la ruta existe
 const rutaExiste = (ruta) => fs.existsSync(ruta);
-console.log('La ruta existe: ---> ', rutaExiste(rutaAbsolutaEjemplo), 15);
+//console.log('La ruta existe: ---> ', rutaExiste(rutaAbsolutaEjemplo), 15);
 
 // si es un archivo - file
 const esArchivo = (ruta) => fs.lstatSync(ruta).isFile();
-console.log('La ruta es un archivo: ---> ', esArchivo(rutaAbsolutaEjemplo), 19); // true
+// console.log('La ruta es un archivo: ---> ', esArchivo(rutaAbsolutaEjemplo), 19); // true
 
 // si es una carpeta - directorio
 const esDirectorio = (ruta) => fs.lstatSync(ruta).isDirectory();
-console.log('La ruta es un directorio: ---> ', esDirectorio(rutaDirectorioEjemplo) , 23) // true
+//console.log('La ruta es un directorio: ---> ', esDirectorio(rutaDirectorioEjemplo) , 23) // true
 
 // si es un archivo .md
 const esMd = (ruta) => path.extname(ruta) === '.md';
-console.log('La ruta tiene de extencion .md: ---> ', esMd(rutaRelativaEjemplo));
+//console.log('La ruta tiene de extencion .md: ---> ', esMd(rutaRelativaEjemplo));
 
 // lee la carpeta - directorio
 const leeDirectorio = (ruta) => fs.readdirSync(ruta);
@@ -33,16 +33,16 @@ console.log('Contenido de la carpeta en un array ---> ', leeDirectorio(rutaDirec
 
 // lee un archivo - file
 const leeArchivo = (ruta) => fs.readFileSync(ruta, 'utf-8');
-console.log('Contenido de la archivo ---> ', leeArchivo(rutaAbsolutaEjemplo));
+ console.log('Contenido de la archivo ---> ', leeArchivo(rutaAbsolutaEjemplo));
 
 // funcion encuentra archivos .md
 // si es un directorio -> lee el contenido del directorio -> si el directorio tiene contenido -> recorre el directorio -> encuentra archivos .md -> lo guarda en un array
 const arrayDeArchivos = [];
 const encontrarArchivosMd = (ruta) => {
     if(esDirectorio(ruta)) {
-    console.log(ruta, '---> Es una carpeta');
+    //console.log(ruta, '---> Es una carpeta');
     const directorioDeArchivos = leeDirectorio(ruta);
-    console.log('Los archivos de la carpeta ---> ', directorioDeArchivos, 87);
+    //console.log('Los archivos de la carpeta ---> ', directorioDeArchivos, 87);
         directorioDeArchivos.forEach((elem) => {
         const rutaElem = elem;
         const nuevaRuta = path.join(ruta, rutaElem);
@@ -67,16 +67,16 @@ const leeEnlacesMd = (archivo) => {
         arrayDeEnlacesMd.push(
             {
                 href: url,
-                text: urlText.substring(0, 50),
+                text: urlText.substring(0,20),
                 ruta: ruta
               }
           );
       };
-      marked(leeArchivosMd, { renderer});
+      marked(leeArchivosMd, {renderer});
   });
   return arrayDeEnlacesMd;
 };
-console.log(leeEnlacesMd('D:\\LABORATORIA2021\\LIM015-md-links\\example\\new\\prueba.md'));
+ console.log(leeEnlacesMd('D:\\LABORATORIA2021\\LIM015-md-links\\example\\new\\prueba.md'));
 
 
 module.exports = {
@@ -87,4 +87,6 @@ module.exports = {
   esMd,
   leeDirectorio,
   leeArchivo,
+  encontrarArchivosMd,
+  leeEnlacesMd,
 };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,11 +1,93 @@
-const api = require('../api.js');
+const api = require('../src/api.js');
 
 const rutaRelativaEjemplo = 'example/README.md';
+const rutaAbsolutaEjemplo = 'D:/LABORATORIA2021/LIM015-md-links/example/todolist.txt';
+const rutaDirectorioEjemplo = 'D:\\LABORATORIA2021\\LIM015-md-links\\example';
+const rutaArchivoMdEjemplo = 'D:\\LABORATORIA2021\\LIM015-md-links\\example\\new\\prueba.md';
 
-describe('Retorna la ruta absoluta', () => {
+describe('API', () => {
+  // funcion que convierte una ruta relativa a absoluta
+  it('Retorna function ',() => {
+    expect(typeof api.rutaAbsoluta).toBe('function');
+  });
+  it('Retorna la ruta absoluta', () => {
+    expect(api.rutaAbsoluta(rutaRelativaEjemplo)).toBe('D:\\LABORATORIA2021\\LIM015-md-links\\example\\README.md');
+  });
+  it('Retorna la ruta absoluta', () => {
+    expect(api.rutaAbsoluta(rutaDirectorioEjemplo)).toBe('D:\\LABORATORIA2021\\LIM015-md-links\\example');
+  });
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  // funcion existe(valida) la ruta
+  it('Retorna function ',() => {
+    expect(typeof api.rutaExiste).toBe('function');
+  });
+  it('Retorna true si la ruta existe', () => {
+    expect(api.rutaExiste(rutaRelativaEjemplo)).toBe(true);
+  });
+
+  // funcion si es un archivo
+  it('Retorna function ',() => {
+    expect(typeof api.esArchivo).toBe('function');
+  });
+  it('Retorna true si es un archivo', () => {
+    expect(api.esArchivo(rutaAbsolutaEjemplo)).toBe(true);
+  });
+
+  // funcion si es un directorio
+  it('Retorna function ',() => {
+    expect(typeof api.esDirectorio).toBe('function');
+  });
+  it('Retorna true si es un directorio', () => {
+    expect(api.esDirectorio(rutaDirectorioEjemplo)).toBe(true);
+  });
+
+  // funcion si es un archivo .md
+  it('Retorna function ',() => {
+    expect(typeof api.esMd).toBe('function');
+  });
+  it('Retorna true si es un archivo .md', () => {
+    expect(api.esMd(rutaRelativaEjemplo)).toBe(true);
+  });
+
+  // funcion lee el directorio
+  it('Retorna function ',() => {
+    expect(typeof api.leeDirectorio).toBe('function');
+  });
+  it('Lee contenido de la carpeta y lo devuelve en un array', () => {
+    expect(typeof api.leeDirectorio(rutaDirectorioEjemplo)).toBe('object');
+  });
+
+  // funcion lee el archivo
+  it('Retorna function', () => {
+    expect(typeof api.leeArchivo).toBe('function');
+  });
+  it('Lee contenido de la carpeta y lo devuelve en un string', () => {
+    expect(typeof api.leeArchivo(rutaArchivoMdEjemplo)).toBe('string');
+  });
+
+  // funcion encontrar archivos .md
+  it('Retorna function', () => {
+    expect(typeof api.encontrarArchivosMd).toBe('function');
+  });
+  it('Encuentra archivos .md y lo devuelve en un array', () => {
+    expect(typeof api.encontrarArchivosMd(rutaDirectorioEjemplo)).toBe('object');
+  });
+
+  // funcion lee los enlaces del archivo .md
+  it('Retorna function', () => {
+    expect(typeof api.leeEnlacesMd).toBe('function');
+  });
+  it('Lee archivos .md y lo devuelve en un array de objetos', () => {
+    expect(typeof api.leeEnlacesMd(rutaArchivoMdEjemplo)).toBe('object');
   });
 
 });
+
+// __tests__/index.test.js
+/*
+describe('initial', () => {
+  test('first tests', () => {
+    expect(true).toBe(true)
+  })
+})*/
+
